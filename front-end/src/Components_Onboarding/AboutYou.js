4fb155425196bfc6ee41f0d24d9e4c18fcf21a32
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2023-01-26 13:21:29 -0500

    adding styling to About You and added date picker with calendar through material UI dayjs package

--- front-end/src/Components_Onboarding/AboutYou.js
+++ front-end/src/Components_Onboarding/AboutYou.js
@@ -1,16 +1,59 @@
 import React from "react";
 import "./AboutYou.scss";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 
 export default function AboutYou() {
+  const [value, setValue] = React.useState(dayjs("2023-01-26T21:11:54"));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div className="onboarding">
       <div className="onboarding__progress">
-        <div className="onboarding__progress1">About You</div>
-        <div className="onboarding__progress2">Goal Weight</div>
-        <div className="onboarding__progress3">Activity Level</div>
-        <div className="onboarding__progress4">Energy Target</div>
-        <div className="onboarding__progress5">Exercise Target</div>
+        <div className="onboarding__progress1">
+          <div className="flex justify-center">
+            <img
+              src={require("../Assets/number-one (1).png")}
+              className="flex justify-center"
+            />
+          </div>
+          About You
+        </div>
+        <hr class="w-14 h-0.5 my-4 bg-gray-400" />
+        <div className="onboarding__progress2">
+          <div className="flex justify-center">
+            <img src={require("../Assets/number-2.png")} />
+          </div>
+          Goal Weight
+        </div>
+        <hr class="w-14 h-0.5 my-4 bg-gray-400" />
+        <div className="onboarding__progress3">
+          <div className="flex justify-center">
+            <img src={require("../Assets/number-3.png")} />
+          </div>
+          Activity Level
+        </div>
+        <hr class="w-14 h-0.5 my-4 bg-gray-400" />
+        <div className="onboarding__progress4">
+          <div className="flex justify-center">
+            <img src={require("../Assets/number-four.png")} />
+          </div>
+          Energy Target
+        </div>
+        <hr class="w-14 h-0.5 my-4 bg-gray-400" />
+        <div className="onboarding__progress5">
+          <div className="flex justify-center">
+            <img src={require("../Assets/number-5.png")} />
+          </div>
+          Exercise Target
+        </div>
       </div>
       <form>
         <div className="onboarding__heading">
@@ -18,7 +61,14 @@ export default function AboutYou() {
         </div>
         <div className="onboarding__about">
           <label>Date of Birth</label>
-          <input></input>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              inputFormat="MM/DD/YYYY"
+              value={value}
+              onChange={handleChange}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
           <label>Gender</label>
           <input></input>
           <label>Height</label>
