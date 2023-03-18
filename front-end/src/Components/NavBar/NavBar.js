51146f49f8f4d59aa4f8a0c3719a0d3c611126b4
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2023-03-18 19:16:37 -0400

    creating hamburger menu for navbar

--- front-end/src/Components/NavBar/NavBar.js
+++ front-end/src/Components/NavBar/NavBar.js
@@ -6,14 +6,19 @@ const Nav = () => {
   const [textColor, setTextColor] = useState("#2A64E5");
   const [backgroundColor, setBackgroundColor] = useState("white");
 
+  //to change burger classes
+  const [burgerClass, setBurgerClass] = useState("burger-bar unclicked")
+  const [menuClass, setMenuClass] = useState("menu hidden")
+  const [isMenuClicked, setisMenuClicked] = useState(false)
+
   useEffect(() => {
     function handleScroll() {
       if (window.scrollY < 600) {
-        setTextColor("#2A64E5");
-        setBackgroundColor("white");
+        setTextColor("white");
+        setBackgroundColor("#222430");
       } else {
         setTextColor("white");
-        setBackgroundColor("black");
+        setBackgroundColor("#222430");
       }
     }
 
@@ -31,6 +36,18 @@ const Nav = () => {
   const background = {
     backgroundColor: backgroundColor,
   };
+
+  const updateMenu = () => {
+    if (!isMenuClicked) {
+      setBurgerClass("burger-bar clicked")
+      setMenuClass("menu visible")
+    } else {
+      setBurgerClass("burger-bar unclicked")
+      setMenuClass("menu hidden")
+    }
+
+    setisMenuClicked(!isMenuClicked)
+  }
   return (
     <nav className="navbar" style={background}>
       <div className="navbar__container">
@@ -38,8 +55,15 @@ const Nav = () => {
         <div style={style} className="navbar__container__logo">
           <a href="/home">BetterHealth</a>
         </div>
-        {/* Navbar Routes*/}
-        <div className="navbar__container__routes">
+        {/* Navbar BurgerMenu*/}
+
+        <div className="navbar__container__burgerMenu" onClick={updateMenu}>
+          <div className={burgerClass}></div>
+          <div className={burgerClass}></div>
+          <div className={burgerClass}></div>
+        </div>
+
+        {/* <div className="navbar__container__routes">
           <div style={style}>
             <a href="/myweight">My Weight</a>
           </div>
@@ -48,8 +72,10 @@ const Nav = () => {
           <div style={style}>
             <a href="/myfoods">My Foods</a>
           </div>
-        </div>
+        </div> */}
       </div>
+
+      <div className={menuClass}></div>
     </nav>
   );
 };
