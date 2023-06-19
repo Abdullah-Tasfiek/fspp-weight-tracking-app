da7ece7f9157c4bf4890d561abba51025b48c3b3
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2023-06-18 23:54:49 -0400

    created and styled nutrition label with table, sass, and tailwind

--- front-end/src/Components/FoodLogger/NutritionLabel.js
+++ front-end/src/Components/FoodLogger/NutritionLabel.js
@@ -14,71 +14,88 @@ export default function NutritionLabel({ loading }) {
           <div className="nutritionLabelBackground__container__subHeading__serving">
             Amount Per Serving
           </div>
-          <div className="nutritionLabelBackground__container__subHeading__calories">
-            Calories{" "}
-            <span className="nutritionLabelBackground__container__subHeading__span">
-              1774
-            </span>
-          </div>
+          <table className="nutritionLabelBackground__container__subHeading__calories">
+            <tbody>
+              <tr>
+                <td className="font-bold">Calories</td>
+                <td className="font-bold text-right">1774</td>
+              </tr>
+            </tbody>
+          </table>
           <div className="nutritionLabelBackground__container__subHeading__divider__bottom"></div>
           <div className="nutritionLabelBackground__container__subHeading__end">
             % Daily Value*
           </div>
         </div>
         <div className="nutritionLabelBackground__container__body">
-          <table className="container__table">
-            <tbody>
+          <table className="nutritionLabelBackground__container__body__table">
+            <tbody className="nutritionLabelBackground__container__body__table__body">
               <tr>
-                <td className="container__table__leeftColumn">Total Fat</td>
-                <td colSpan={1}>18.3g</td>
-                <td colSpan={2} className="container__table___wideColumn">28%</td>
+                <td>
+                  <span className="font-bold">Total Fat</span> 18.3 g
+                </td>
+                <td className="font-bold text-right">28 %</td>
               </tr>
             </tbody>
-            <div className="nutritionLabelBackground__container__body__divider"></div>
-            {/* <tbody>
-              <tr>
-                <td>1</td>
-                <td>Piece</td>
-                <td className="container__table___wideColumn">Apple</td>
-              </tr>
-            </tbody> */}
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td className="tex-right">Saturated Fat 2 g</td>
+              <td className="font-bold text-right">10 %</td>
+            </tr>
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td className="tex-right">Trans Fat 0 g</td>
+              {/* <td className="font-bold text-right">10%</td> */}
+            </tr>
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td>
+                <span className="font-bold">Cholestrol</span> 0 mg
+              </td>
+              <td className="font-bold text-right">0 %</td>
+            </tr>
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td>
+                <span className="font-bold">Sodium</span> 70 mg
+              </td>
+              <td className="font-bold text-right">3 %</td>
+            </tr>
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td>
+                <span className="font-bold">Total Carbohydrates</span> 333.2 g
+              </td>
+              <td className="font-bold text-right">111 %</td>
+            </tr>
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td className="tex-right">Dietary Fiber 34.6 g</td>
+              <td className="font-bold text-right">138 %</td>
+            </tr>
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td className="tex-right">Total Sugars 30.3 g</td>
+            </tr>
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td className="tex-right">Includes - Added Sugars</td>
+            </tr>
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td>
+                <span className="font-bold">Protein</span> 71 g
+              </td>
+              <td className="font-bold text-right">142 %</td>
+            </tr>
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td className="tex-right">Vitamin D 0 µg</td>
+              <td className="font-bold text-right">0%</td>
+            </tr>
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td className="tex-right">Calcium 179.1 mg</td>
+              <td className="font-bold text-right">18%</td>
+            </tr>
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td className="tex-right">Iron 13.8 mg</td>
+              <td className="font-bold text-right">77 %</td>
+            </tr>
+            <tr className="nutritionLabelBackground__container__body__table__body__new">
+              <td className="tex-right">Potassium 2203.2 mg</td>
+              <td className="font-bold text-right">47 %</td>
+            </tr>
           </table>
-          {/* <div>
-            Total Fat 18.3g <span>28%</span>
-          </div>
-          <div>
-            Saturated Fat 2g <span>10%</span>
-          </div>
-          <div>Trans Fat 18.3g</div>
-          <div>
-            Cholestrol <span>0mg</span> <span>0%</span>
-          </div>
-          <div>
-            Sodium <span>70mg</span> <span>0%</span>
-          </div>
-          <div>
-            Total Carbohydrates <span>50g</span> <span>50%</span>
-          </div>
-          <div>
-            Dietary Fiber 6g <span>20%</span>
-          </div>
-          <div>Total Sugars 30g</div>
-          <div>Includes - Added Sugars</div>
-          <div>
-            Protein 142g<span>80%</span>
-          </div>
-          <div>
-            Vitamin D 0 µg<span>0%</span>
-          </div>
-          <div>
-            Calcium 179.1 mg <span>18%</span>
-          </div>
-          <div>
-            Iron 179.1 mg <span>18%</span>
-          </div>
-          <div>
-            Potassium 179.1 mg <span>18%</span>
-          </div> */}
         </div>
         <div className="nutritionLabelBackground__container__footer">
           *Percent Daily Values are based on a 2000 calorie diet
