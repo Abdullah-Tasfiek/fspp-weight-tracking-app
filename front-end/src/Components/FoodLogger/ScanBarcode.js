ef07f89caff01c60e9187142cc87a9b6138a1087
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2023-07-10 15:53:59 -0400

    setting up barcode scanning, seems to work on desktop. Will check on mobile once pushed to production

--- front-end/src/Components/FoodLogger/ScanBarcode.js
+++ front-end/src/Components/FoodLogger/ScanBarcode.js
@@ -1,9 +1,59 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Quagga from "quagga";
-import "./ScanBarcode.scss";
+import axios from "axios";
 
-export default function ScanBarcode() {
+const ScanBarcodeButton = () => {
+  const [barcodeID, setBarcodeID] = useState("");
+  const [productData, setProductData] = useState(null);
   const fileInputRef = useRef(null);
+  const videoRef = useRef(null);
+
+  const BARCODE_API = `https://world.openfoodfacts.org/api/v0/product/${barcodeID}.json`;
+
+  useEffect(() => {
+    Quagga.init(
+      {
+        inputStream: {
+          type: "LiveStream",
+          constraints: {
+            width: 640,
+            height: 480,
+            facingMode: "environment", // or 'user' for the front camera
+          },
+          target: videoRef.current,
+        },
+        decoder: {
+          readers: ["ean_reader"], // or other supported barcode formats
+        },
+      },
+      function (err) {
+        if (err) {
+          console.error("Error initializing Quagga:", err);
+        } else {
+          console.log("Quagga initialized successfully");
+          Quagga.start();
+        }
+      }
+    );
+
+    Quagga.onDetected(handleBarcodeScan);
+
+    return () => {
+      Quagga.stop();
+      Quagga.offDetected(handleBarcodeScan);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (barcodeID) {
+      fetchProductData();
+    }
+  }, [barcodeID]);
+
+  const handleBarcodeScan = (result) => {
+    console.log("Barcode scanned:", result.codeResult.code);
+    setBarcodeID(result.codeResult.code);
+  };
 
   const handleButtonClick = () => {
     if (fileInputRef.current) {
@@ -13,8 +63,39 @@ export default function ScanBarcode() {
 
   const handleFileInputChange = (event) => {
     const file = event.target.files[0];
-    // Handle the selected file (image) here
-    console.log("Selected file:", file);
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        Quagga.decodeSingle(
+          {
+            src: e.target.result,
+            numOfWorkers: navigator.hardwareConcurrency || 1,
+            locate: true,
+            decoder: {
+              readers: ["ean_reader"], // or other supported barcode formats
+            },
+          },
+          function (result) {
+            if (result && result.codeResult) {
+              console.log("Barcode scanned:", result.codeResult.code);
+              setBarcodeID(result.codeResult.code);
+            } else {
+              console.log("No barcode found");
+            }
+          }
+        );
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const fetchProductData = async () => {
+    try {
+      const response = await axios.get(BARCODE_API);
+      setProductData(response.data);
+    } catch (error) {
+      console.error("Error fetching product data:", error);
+    }
   };
 
   return (
@@ -28,6 +109,24 @@ export default function ScanBarcode() {
         style={{ display: "none" }}
         onChange={handleFileInputChange}
       />
+      <video
+        ref={videoRef}
+        width="100%"
+        height="auto"
+        style={{ display: "none" }}
+      ></video>
+
+      {productData && (
+        <div>
+          <h2>Product Information</h2>
+          <p>Product Name: {productData.product.product_name}</p>
+          <p>Brand: {productData.product.brands}</p>
+          <p>Ingredients: {productData.product.ingredients_text}</p>
+          {/* Render other desired product information */}
+        </div>
+      )}
     </div>
   );
-}
+};
+
+export default ScanBarcodeButton;
