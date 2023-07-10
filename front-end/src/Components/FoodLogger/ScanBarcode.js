dbcbe7b59bf6705cb80337f21aa1bddcf2a33e58
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2023-07-10 16:44:53 -0400

    barcode input and scanning

--- front-end/src/Components/FoodLogger/ScanBarcode.js
+++ front-end/src/Components/FoodLogger/ScanBarcode.js
@@ -1,59 +1,15 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 import Quagga from "quagga";
 import axios from "axios";
+import Loader from "./Loader";
 
-const ScanBarcodeButton = () => {
+const ScanBarcode = () => {
   const [barcodeID, setBarcodeID] = useState("");
   const [productData, setProductData] = useState(null);
+  const [loading, setLoading] = useState(false);
   const fileInputRef = useRef(null);
-  const videoRef = useRef(null);
 
   const BARCODE_API = `https://world.openfoodfacts.org/api/v0/product/${barcodeID}.json`;
-
-  useEffect(() => {
-    Quagga.init(
-      {
-        inputStream: {
-          type: "LiveStream",
-          constraints: {
-            width: 640,
-            height: 480,
-            facingMode: "environment", // or 'user' for the front camera
-          },
-          target: videoRef.current,
-        },
-        decoder: {
-          readers: ["ean_reader"], // or other supported barcode formats
-        },
-      },
-      function (err) {
-        if (err) {
-          console.error("Error initializing Quagga:", err);
-        } else {
-          console.log("Quagga initialized successfully");
-          Quagga.start();
-        }
-      }
-    );
-
-    Quagga.onDetected(handleBarcodeScan);
-
-    return () => {
-      Quagga.stop();
-      Quagga.offDetected(handleBarcodeScan);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (barcodeID) {
-      fetchProductData();
-    }
-  }, [barcodeID]);
-
-  const handleBarcodeScan = (result) => {
-    console.log("Barcode scanned:", result.codeResult.code);
-    setBarcodeID(result.codeResult.code);
-  };
 
   const handleButtonClick = () => {
     if (fileInputRef.current) {
@@ -65,41 +21,114 @@ const ScanBarcodeButton = () => {
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
-      reader.onload = function (e) {
-        Quagga.decodeSingle(
-          {
-            src: e.target.result,
-            numOfWorkers: navigator.hardwareConcurrency || 1,
-            locate: true,
-            decoder: {
-              readers: ["ean_reader"], // or other supported barcode formats
-            },
-          },
-          function (result) {
-            if (result && result.codeResult) {
-              console.log("Barcode scanned:", result.codeResult.code);
-              setBarcodeID(result.codeResult.code);
-            } else {
-              console.log("No barcode found");
-            }
+      reader.onload = async function (e) {
+        const imageDataUrl = e.target.result;
+        try {
+          const result = await decodeBarcode(imageDataUrl);
+          if (result && result.codeResult) {
+            console.log("Barcode scanned:", result.codeResult.code);
+            setBarcodeID(result.codeResult.code);
+            fetchProductData(result.codeResult.code);
+          } else {
+            console.log("No barcode found");
+            setBarcodeID("");
           }
-        );
+        } catch (error) {
+          console.error("Error decoding barcode:", error);
+          setBarcodeID("");
+        }
       };
       reader.readAsDataURL(file);
     }
   };
 
-  const fetchProductData = async () => {
+  const decodeBarcode = (imageDataUrl) => {
+    return new Promise((resolve, reject) => {
+      Quagga.decodeSingle(
+        {
+          src: imageDataUrl,
+          numOfWorkers: navigator.hardwareConcurrency || 1,
+          locate: true,
+          decoder: {
+            readers: ["ean_reader"], // or other supported barcode formats
+          },
+        },
+        function (result) {
+          resolve(result);
+        }
+      );
+    });
+  };
+
+  const handleBarcodeSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
     try {
-      const response = await axios.get(BARCODE_API);
-      setProductData(response.data);
+      await fetchProductData(barcodeID);
     } catch (error) {
       console.error("Error fetching product data:", error);
+    } finally {
+      setLoading(false);
     }
+  };
+
+  const fetchProductData = async (barcode) => {
+    try {
+      const response = await axios.get(
+        `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`
+      );
+      setProductData(response.data);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const renderProductInfo = () => {
+    if (productData) {
+      const {
+        product_name,
+        nutriments: {
+          energy_value,
+          fat_value,
+          "saturated-fat_value": saturatedFat_value,
+          carbohydrates_value,
+          sugars_value,
+          fiber_value,
+          proteins_value,
+          sodium_value,
+        },
+      } = productData.product;
+
+      return (
+        <div className="product-info">
+          <h2>Product Information</h2>
+          <p>Product Name: {product_name}</p>
+          <p>Energy (kcal) per serving: {energy_value}</p>
+          <p>Fat per serving: {fat_value}</p>
+          <p>Saturated Fat per serving: {saturatedFat_value}</p>
+          <p>Carbohydrates per serving: {carbohydrates_value}</p>
+          <p>Sugars per serving: {sugars_value}</p>
+          <p>Fiber per serving: {fiber_value}</p>
+          <p>Proteins per serving: {proteins_value}</p>
+          <p>Sodium per serving: {sodium_value}</p>
+        </div>
+      );
+    }
+    return null;
   };
 
   return (
     <div>
+      <form onSubmit={handleBarcodeSubmit}>
+        <input
+          type="text"
+          placeholder="Enter barcode number"
+          value={barcodeID}
+          onChange={(e) => setBarcodeID(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+
       <button onClick={handleButtonClick}>Scan Barcode</button>
       <input
         ref={fileInputRef}
@@ -109,24 +138,12 @@ const ScanBarcodeButton = () => {
         style={{ display: "none" }}
         onChange={handleFileInputChange}
       />
-      <video
-        ref={videoRef}
-        width="100%"
-        height="auto"
-        style={{ display: "none" }}
-      ></video>
 
-      {productData && (
-        <div>
-          <h2>Product Information</h2>
-          <p>Product Name: {productData.product.product_name}</p>
-          <p>Brand: {productData.product.brands}</p>
-          <p>Ingredients: {productData.product.ingredients_text}</p>
-          {/* Render other desired product information */}
-        </div>
-      )}
+      <Loader loading={loading} message="Retrieving product data..." />
+
+      {productData && renderProductInfo()}
     </div>
   );
 };
 
-export default ScanBarcodeButton;
+export default ScanBarcode;
